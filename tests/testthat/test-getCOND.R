7e563826d85290f6bc5c716cad75142435eef482
steven2249 <stevenysw@berkeley.edu>
2017-04-22 15:22:06 -0700

    Update test-getCOND.R

--- tests/testthat/test-getCOND.R
+++ tests/testthat/test-getCOND.R
@@ -1,6 +1,11 @@
 context("getCOND")
 
 test_that("getCOND", {
-  tt <- getCOND()
-  
+  tt <- getCOND("fugus")
+ 
+  expect_is(tt, "list")
+  expect_is(tt$sp, "integer")
+  expect_is(tt$hfu, "integer")
+  expect_is(tt$hf.st, "integer")
+  expect_is(tt$spe.st, "integer")  
 })
