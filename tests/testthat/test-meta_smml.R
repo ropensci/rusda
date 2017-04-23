035616b0c611caeb34651c435cb219b93f1feb26
steven2249 <stevenysw@berkeley.edu>
2017-04-23 02:19:23 -0700

    Update and rename test-rusda::meta_smml.R to test-meta_smml.R

---
+++ tests/testthat/test-meta_smml.R
@@ -0,0 +1,5 @@
+context("meta_smml")
+
+test_that("meta_smml", {
+  tt <- meta_smml()
+})
--- tests/testthat/test-rusda::meta_smml.R
+++
@@ -1,5 +0,0 @@
-context("rusda::meta_smml")
-
-test_that("rusda::meta_smml", {
-  tt <- rusda::meta_smml()
-})
