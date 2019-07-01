69293b140605ae50adec9e1616a5e83ff57129ce
Franz <f.krah@mailbox.org>
2019-07-01 17:48:17 +0200

    modify getStudy

--- R/getStudy.R
+++ R/getStudy.R
@@ -5,22 +5,16 @@
 #' 
 #' @return an object of class \code{list} with studies
 #' 
+#' @ import stringr
 #' @author Franz-Sebastian Krah
 #' @export
 
-
-getStudy_single <- function(id = 42264){
-url <- paste0("https://nt.ars-grin.gov/fungaldatabases/fungushost/new_rptOneLit.cfm?fungRec=", id, "&thisError=")
-
-pars <- rawToChar(GET(url)$content)
-pars <- htmlTreeParse(pars, useInternalNodes = TRUE)
-pars <- xpathApply(pars, "//p", xmlValue)
-pars <- unlist(pars)
-res <- pars[[1]]
-return(res)
-}
-
-getStudy <- function(id = c(94, 42264, 6135)){
-  res <- lapply(id, getStudy_single)
+getStudy <- function(x, sep = "; ") {
+  x <- as.character(x)
+  x <- str_extract_all(x, "\\d*")[[1]]
+  x <- x[str_detect(x, "\\d+")]
+  res <- paste(unlist(lapply(x, getStudy_single)), collapse = sep)
   return(res)
 }
+
+
